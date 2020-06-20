229b6dc766a5b197f316546baeaa642d5888d4c4
SerginhoLeal <48488987+SerginhoLeal@users.noreply.github.com>
2020-06-19 23:48:34 -0300

    pedido

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -16,4 +16,17 @@ module.exports = {
 
         return res.json({sens});
     },
+    async indexPerfil(req, res){
+        const {pedido} = req.query;
+
+        const desc2 = parseStrings(pedido);
+
+        const sens = await Sea.find({
+            pedido:{
+                $in:desc2,
+            },
+        });
+
+        return res.json({sens});
+    },
 }
--- src/routes.js
+++ src/routes.js
@@ -15,6 +15,7 @@ routes.get('/OTT7RrH1TRC7Ypo2iYtR', ProductController.index);
 routes.post('/C7Ypo2iFU0OTT7RrH1TR', ProductController.store);
 
 routes.get('/1TT75a8s5AS88aAo2iYtR', SearchController.index);
+routes.get('/Ao1TT75a8s5AS88a2iYtR', SearchController.indexPerfil);
 
 routes.get('/S881TT75a8s5AaAo2iYtR', PastaController.index);
 routes.post('/C1TT75a8s5AOTT7RH1TR', PastaController.store);
